ac1904646c8729d4077442a65f499815eb2d6286
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2020-09-02 05:10:30 +0900

    chapter7 image & csv

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -2,7 +2,13 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
 
   def index
-    @tasks = current_user.tasks.order(created_at: :desc)
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv" }
+    end
   end
 
   def show
@@ -12,10 +18,22 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
   def create
     @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
-      redirect_to tasks_url, notice: "タスク「 #{@task.name}」を登録しました。 "
+      TaskMailer.creation_email(@task).deliver_now
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
     end
@@ -34,9 +52,14 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク [#{@task.name}]を削除しました。"
   end
 
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
+  end
+
   private
     def task_params
-      params.require(:task).permit(:name, :description)
+      params.require(:task).permit(:name, :description, :image)
     end
 
     def set_task
